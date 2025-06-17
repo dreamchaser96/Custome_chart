a9af5eb8108a0a1048e01676891e2312172d413d
dreamchaser <dc.info.apps@gmail.com>
2025-06-17 16:03:43 +0300

    Update chart_title.dart

--- lib/src/behaviors/chart_title/chart_title.dart
+++ lib/src/behaviors/chart_title/chart_title.dart
@@ -23,6 +23,7 @@ import 'package:charts_common/common.dart' as common
         OutsideJustification,
         TextStyleSpec;
 import 'package:flutter/foundation.dart' show hashValues;
+import 'package:flutter/widgets.dart';                      
 import 'package:meta/meta.dart' show immutable;
 import '../chart_behavior.dart' show ChartBehavior, GestureType;
 
