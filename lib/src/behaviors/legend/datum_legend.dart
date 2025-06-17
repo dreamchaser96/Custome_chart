f1ace9095ea08330ed9d617b6a6feff79e5c534b
dreamchaser <dc.info.apps@gmail.com>
2025-06-17 15:51:09 +0300

    Update datum_legend.dart

--- lib/src/behaviors/legend/datum_legend.dart
+++ lib/src/behaviors/legend/datum_legend.dart
@@ -25,7 +25,7 @@ import 'package:charts_common/common.dart' as common
         OutsideJustification,
         SelectionModelType,
         TextStyleSpec;
-import 'package:flutter/widgets.dart'
+import 'package:flutter/foundation.dart'
     show BuildContext, EdgeInsets, Widget, hashValues;
 import 'package:meta/meta.dart' show immutable;
 import '../../chart_container.dart' show ChartContainerRenderObject;
